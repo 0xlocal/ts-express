518ba0f3d2058b1a7b13a2e6f2b23bf97f457bb1
Ferrel John Fernando <f.syntaxer@gmail.com>
2021-12-15 02:57:17 +0700

    update update author and category controller

--- src/controller/author.controller.ts
+++ src/controller/author.controller.ts
@@ -5,6 +5,7 @@ import { AuthorService } from "../service/author.service";
 import { AuthorDTO } from "../dto/author.dto";
 import authMiddleware from "../middleware/auth.middleware";
 import validationMiddleware from "../middleware/validation.middleware";
+import HttpException from "../exception/http.exception";
 
 export class AuthorController implements Controller {
   public path: string = "/authors";
@@ -23,34 +24,45 @@ export class AuthorController implements Controller {
     res.json(authors);
   };
 
-  private getOne = async (req: Request, res: Response) => {
+  private getOne = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
     const author = await this.authorService.getOne(Number(id));
 
-    res.json(author);
+    if (author) {
+      res.json(author);
+    } else {
+      next(new HttpException(404, `Author with id ${id} not found`));
+    }
   };
 
-  private create = async (req: Request, res: Response) => {
-    const author = req.body as Author;
-    const newAuthor = await this.authorService.create(author);
+  private create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const author = req.body as Author;
+      const newAuthor = await this.authorService.create(author);
 
-    res.status(201).json(newAuthor);
+      res.status(201).json(newAuthor);
+    } catch (error) {
+      next(error);
+    }
   };
 
-  private update = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const author = req.body as Author;
+  private update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.id;
+      const author = req.body as Author;
+      const updatedAuthor = await this.authorService.update(author, Number(id));
 
-    const updatedAuthor = await this.authorService.update(author, Number(id));
-
-    res.json(updatedAuthor);
+      res.json(updatedAuthor);
+    } catch (error) {
+      next(error);
+    }
   };
 
   private delete = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const id = req.params.id;
-
       await this.authorService.delete(Number(id));
+
       res.status(200).json();
     } catch (error) {
       next(error);
@@ -59,7 +71,7 @@ export class AuthorController implements Controller {
 
   public routes() {
     this.router
-      // .all(`${this.path}/*`, authMiddleware)
+      .all(`${this.path}/*`, authMiddleware)
       .get(this.path, this.index)
       .get(`${this.path}/:id`, this.getOne)
       .post(this.path, validationMiddleware(AuthorDTO), this.create)
--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -5,6 +5,7 @@ import { CategoryService } from "../service/category.service";
 import authMiddleware from "../middleware/auth.middleware";
 import validationMiddleware from "../middleware/validation.middleware";
 import { CategoryDTO } from "../dto/category.dto";
+import HttpException from "../exception/http.exception";
 
 export class CategoryController implements Controller {
   public path: string = "/categories";
@@ -18,42 +19,60 @@ export class CategoryController implements Controller {
   }
 
   private index = async (req: Request, res: Response) => {
+    // * slower than await method
+    // this.categoryService.index().then((categories) => res.json(categories));
     const categories = await this.categoryService.index();
-
     res.json(categories);
   };
 
-  private getOne = async (req: Request, res: Response) => {
+  private getOne = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
     const category = await this.categoryService.getOne(Number(id));
 
-    res.json(category);
+    if (category) {
+      res.json(category);
+    } else {
+      next(new HttpException(404, `Category with id ${id} not found`));
+    }
   };
 
-  private create = async (req: Request, res: Response) => {
-    const category = req.body as Category;
-    const newCategory = await this.categoryService.create(category);
+  private create = async (req: Request, res: Response, next: NextFunction) => {
+    // * slower than await method
+    // const category = req.body as Category;
+    // this.categoryService
+    //   .create(category)
+    //   .then((newCategory) => res.json(newCategory))
+    //   .catch((error) => next(error));
+    try {
+      const category = req.body as Category;
+      const newCategory = await this.categoryService.create(category);
 
-    res.status(201).json(newCategory);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
   };
 
-  private update = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const category = req.body as Category;
+  private update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.id;
+      const category = req.body as Category;
+      const updatedCategory = await this.categoryService.update(
+        category,
+        Number(id)
+      );
 
-    const updatedCategory = await this.categoryService.update(
-      category,
-      Number(id)
-    );
-
-    res.json(updatedCategory);
+      res.json(updatedCategory);
+    } catch (error) {
+      next(error);
+    }
   };
 
   private delete = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const id = req.params.id;
-
       await this.categoryService.delete(Number(id));
+
       res.status(200).json();
     } catch (error) {
       next(error);
@@ -62,7 +81,7 @@ export class CategoryController implements Controller {
 
   public routes() {
     this.router
-      // .all(`${this.path}/*`, authMiddleware)
+      .all(`${this.path}/*`, authMiddleware)
       .get(this.path, this.index)
       .get(`${this.path}/:id`, this.getOne)
       .post(this.path, validationMiddleware(CategoryDTO), this.create)
